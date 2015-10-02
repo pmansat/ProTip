24438d0c03f00bac9cc5f899ca335dba93ad0df3
Promeet Mansata <pmansat@l-sb811l7g8w-m.local>
2015-10-02 09:47:08 -0700

    Test Code

--- ProTip/ViewController.swift
+++ ProTip/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tipLabel.text = "$0.00"
+        totalLabel.text="$0.00"
         // Do any additional setup after loading the view, typically from a nib.
     }
+   
 
-    override func didReceiveMemoryWarning() {
+        override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18,0.2,0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tip = billAmount * 0.2
+        var total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+         }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
